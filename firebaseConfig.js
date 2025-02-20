18f58714becece140214a943f2d8107fdd60009b
LuisAlberto2304 <0323105921@ut-tijuana.edu.mx>
2025-02-19 21:52:06 -0800

    firestore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyANIW_Rjj0rBYYONuWLYnkpo5qm50u2uSI",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { auth };
