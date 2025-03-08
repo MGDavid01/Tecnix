ba43c71e7ae7d8d5e77a3e1f36a157394c30c18c
MGDavid01 <0322103760@ut-tijuana.edu.mx>
2025-03-08 10:52:57 -0800

    Arreglo de babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
     ],
-  };
+  ],
+};
